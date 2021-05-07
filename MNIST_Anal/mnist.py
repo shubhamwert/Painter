548e01fbf4cee67e7442b4b38920e5d3071b0d08
shubham <shubhamwert@gmail.com>
2021-05-07 18:38:35 +0530

    test commit

--- MNIST_Anal/mnist.py
+++ MNIST_Anal/mnist.py
@@ -9,24 +9,32 @@ import tensorflow.keras.regularizers as R
 X_train.shape
 X_train=X_train/255.
 X_train=X_train.reshape([-1,28,28,1])
+
+
 def Model(in_c,o_c):
+    r=tf.keras.regularizers.L1L2(l1=0.01, l2=0.01)
     input_=L.Input(shape=in_c)   
-    x=L.Conv2D(in_c[0]*2,kernel_size=3,
-                    activation='relu',
+    x=L.Conv2D(in_c[-1]*8,kernel_size=3,
+                    activation='tanh',
                     padding='same',
                     )(input_)
     x=L.BatchNormalization()(x)
     x=L.MaxPool2D()(x)
-    x=L.Conv2D(in_c[0]*4,kernel_size=3,activation='relu')(x)
-    x=L.Conv2D(in_c[0]*8,kernel_size=3,activation='relu')(x)
+    x=L.Conv2D(in_c[-1]*16,kernel_size=3,activation='relu',kernel_regularizer=r)(x)
+    x=L.AvgPool2D()(x)
+    
+    x=L.Conv2D(in_c[-1]*32,kernel_size=3,activation='relu',kernel_regularizer=r)(x)
     x=L.MaxPool2D()(x)
     x=L.ReLU()(x)
     x=L.Dropout(0.3)(x)
     x=L.Flatten()(x)
-    output=L.Dense(o_c,activation='softmax')(x)
+    output=L.Dense(o_c,activation='softmax',kernel_regularizer=r)(x)
     model=K.Model(inputs=[input_],outputs=[output])
     
     return model
+
+
+
 m=Model([28,28,1],10)
 
 
--- MNIST_Anal/mnist_test.py
+++ MNIST_Anal/mnist_test.py
@@ -1,5 +1,17 @@
 import tensorflow as tf
-
+import tensorflow.keras as K
+import tensorflow.keras.datasets.mnist as mn
+(X_train,Y_train),(X_test,Y_test)=mn.load_data()
+X_test=X_test/255.
+X_test.shape
+X_test=X_test.reshape([-1,28,28,1])
 m=tf.keras.models.load_model('./model')
+m.compile(
+            loss=K.losses.SparseCategoricalCrossentropy(from_logits=False),
+            optimizer=K.optimizers.Adam(lr=0.001),
+            metrics=['sparse_categorical_accuracy']
 
-print(m.summary())
+
+
+)
+print(m.evaluate(X_test,Y_test,batch_size=128))
